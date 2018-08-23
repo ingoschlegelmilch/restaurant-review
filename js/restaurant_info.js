920942f2861820937b550486bb189848fd8466d0
Ingo Schlegelmilch <ingoschlegelmilch@gmail.com>
2018-08-23 15:07:55 +0200

    several adjustments in order to resubmit

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
-  image.setAttribute('alt', restaurant.alt);
+  image.setAttribute('alt', `Image of ${restaurant.name} restaurant`);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
